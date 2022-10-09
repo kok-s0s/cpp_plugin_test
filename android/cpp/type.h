e53ca6072870e91be59f42a00ddd5e8d6efd08d1
kok-s0s <2694308562@qq.com>
2022-10-09 21:13:08 +0800

    feat: test Integer and Struct

--- android/cpp/type.h
+++ android/cpp/type.h
@@ -2,5 +2,44 @@
 
 extern "C"
 {
+  #pragma region Integer
+
+  int8_t type_int8();
+  int16_t type_int16();
   int32_t type_int32();
+  int64_t type_int64();
+
+  uint8_t type_uint8();
+  uint16_t type_uint16();
+  uint32_t type_uint32();
+  uint64_t type_uint64();
+
+  int type_int();
+  long type_long();
+  long long type_long_long();
+  short type_short();
+
+  unsigned int type_unsigned_int();
+  unsigned long type_unsigned_long();
+  unsigned long long type_unsigned_long_long();
+  unsigned short type_unsigned_short();
+
+  #pragma endregion
+
+  #pragma region Struct
+
+  typedef struct {
+    char *name;
+    int age;
+    double weight;
+    double height;
+  } Person;
+
+  Person init_person(char *name, int age, double weight, double height);
+
+  Person *init_person_pointer(char *name, int age, double weight, double height);
+
+  void change_person_age(Person *person);
+
+  #pragma endregion
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -18,6 +21,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   final _cppPluginTestPlugin = CppPluginTest();
+
+  Person personOne =
+      initPerson("Baby".toNativeUtf8().cast<Int8>(), 2, 12.2, 34.2);
+
+  Pointer<Person> personTwo =
+      initPersonPointer("man".toNativeUtf8().cast<Int8>(), 33, 66.6, 172.6);
+
+  void _doubleAge() {
+    setState(() {
+      changePersonAge(personTwo);
+      if (personTwo.ref.age == 0 || personTwo.ref.age < 0) {
+        personTwo.ref.age = 1;
+      }
+    });
+  }
 
   @override
   void initState() {
@@ -52,15 +70,54 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('CPP Plugin Test'),
         ),
         body: Center(
             child: Column(
-          children: [
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             Text('Running on: $_platformVersion\n'),
-            Text('typs_int32: ${typeInt32()}'),
+            // Text('typs_int8: ${typeInt8()}\n'),
+            // Text('typs_int16: ${typeInt16()}\n'),
+            // Text('typs_int32: ${typeInt32()}\n'),
+            // Text('typs_int64: ${typeInt64()}\n'),
+            // Text('typs_uint8: ${typeUint8()}\n'),
+            // Text('typs_uint16: ${typeUint16()}\n'),
+            // Text('typs_uint32: ${typeUint32()}\n'),
+            // Text('typs_uint64: ${typeUint64()}\n'),
+            // Text('typs_int: ${typeInt()}\n'),
+            // Text('typs_long: ${typeLong()}\n'),
+            // Text('typs_long_long: ${typeLongLong()}\n'),
+            // Text('typs_short: ${typeShort()}\n'),
+            // Text('typs_unsigned_int: ${typeUnsignedInt()}\n'),
+            // Text('typs_unsigned_long: ${typeUnsignedLong()}\n'),
+            // Text('typs_unsigned_long_long: ${typeUnsignedLongLong()}\n'),
+            // Text('typs_unsigned_short: ${typeUnsignedShort()}\n'),
+            Text(
+                'personOneName = ${personOne.name.cast<Utf8>().toDartString()}\n'),
+            Text('personOneAge = ${personOne.age}\n'),
+            Text('personOneWeight = ${personOne.weight}\n'),
+            Text('personOneHeight = ${personOne.height}\n'),
+            Text(
+                '(Pointer) personTwoName = ${personTwo.ref.name.cast<Utf8>().toDartString()}\n'),
+            Text('(Pointer) personTwoAge = ${personTwo.ref.age}\n'),
+            Text('(Pointer) personTwoWeight = ${personTwo.ref.weight}\n'),
+            Text('(Pointer) personTwoHeight = ${personTwo.ref.height}\n'),
+            const Text(
+              'after change personTwoAge, now age is \n',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '${personTwo.ref.age}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _doubleAge,
+          tooltip: 'add Age',
+          child: const Icon(Icons.add),
+        ), //
       ),
     );
   }
